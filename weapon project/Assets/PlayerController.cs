3b0f2310c68d747e258a8e190cda5f66f14beb9a
João Farroba <148582269+joaofarroba@users.noreply.github.com>
2025-01-18 03:10:51 +0000

    Player Movement

---
+++ weapon project/Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+
+
+    public float movSpeed;
+    float speedX, speedY;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+    
+        Vector2 movement = new Vector2(inputX, inputY);
+
+        movement = movement.normalized * movSpeed;
+
+        rb.linearVelocity = movement;
+    }
+}
