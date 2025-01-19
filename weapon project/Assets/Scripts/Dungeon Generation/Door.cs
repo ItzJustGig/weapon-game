131b7cec485349db79162d95a0cb8e16781a3d6a
Eusébio Alves <eusebio.a95@hotmail.com>
2025-01-19 14:34:03 +0000

    Camera smooth transition

--- weapon project/Assets/Scripts/Dungeon Generation/Door.cs
+++ weapon project/Assets/Scripts/Dungeon Generation/Door.cs
@@ -1,14 +1,28 @@
 using UnityEngine;
+using UnityEngine.InputSystem.Controls;
 
 public class Door : MonoBehaviour
 {
+    [SerializeField] static private float cameraMoveSpeed = 125.0f;
     public Vector3 myDestinationPos; // A posição central do quarto a que esta porta lida.
-    private GameObject camera; // Referência à camera para mudar a posição dela quando player muda de quarto
-    private GameObject player; // Referência ao player para mudar a posição dele quando mudar de quarto
+    static private GameObject camera; // Referência à camera para mudar a posição dela quando player muda de quarto
+    static private GameObject player; // Referência ao player para mudar a posição dele quando mudar de quarto
+    private bool moveCamera = false;
 
     private void Start() {
         camera = GameObject.FindWithTag("MainCamera");
         player = GameObject.FindWithTag("Player");
+    }
+
+    private void Update() 
+    {
+        if (moveCamera)
+        {
+            camera.transform.position = Vector3.MoveTowards(camera.transform.position , new Vector3(myDestinationPos.x, myDestinationPos.y, -10), cameraMoveSpeed * Time.deltaTime);
+            
+            if (camera.transform.position == new Vector3(myDestinationPos.x, myDestinationPos.y, -10))
+                moveCamera = false;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
@@ -20,7 +34,8 @@ public class Door : MonoBehaviour
 
     private void ChangeRooms()
     {
-        camera.transform.position = new Vector3(myDestinationPos.x, myDestinationPos.y, -10);
+        moveCamera = true;
+        //camera.transform.position = new Vector3(myDestinationPos.x, myDestinationPos.y, -10);
         player.transform.position = myDestinationPos;
     }
 }
