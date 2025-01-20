c016f7772effc00b2e1e7f413ba1e3a709bb375f
Tiago21700980 <tiagobjames@gmail.com>
2025-01-20 03:50:16 +0000

    Second Iterarion - Enemy Controller
    Made a new script called Enemy Controller

--- weapon project/Assets/EnemieBehaviour.cs
+++ weapon project/Assets/EnemieBehaviour.cs
@@ -7,30 +7,42 @@ public class EnemieBehaviour : MonoBehaviour
     public GameObject player; // este vai ser o target do enemie
     public float speed; // velocidade com que o enemie se move
     public float directionChangeInterval = 2f; // intervalo para mudar de direção
+    public float idleEnemieTimeInterval = 3f; // tempo parado
+    public float movingEnemieTimeInterval = 2f; // tempo a mexer-se
     public float AggroRange;
 
     private float distance; // variavel utilizada para calcular a distancia entre este objecto e o seu target(player)
 
     private Vector2 randomDirection; // Variavel para guardar a direção random que este objecto vai tomar
+
+    //Timers
     private float timeSinceLastDirectionChange = 0;
+    private float timeIdle = 0;
+    private float timeMoving = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+    timeSinceLastDirectionChange = 0;
+        timeIdle = 0;
+        timeMoving = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //Timers Update Here
         timeSinceLastDirectionChange += Time.deltaTime;
+        //Debug.Log("Current timeSinceLastDirectionChange: " + timeSinceLastDirectionChange);
 
-        if (timeSinceLastDirectionChange >= directionChangeInterval)
-        {
-            ChangeRandomDirection();
-            timeSinceLastDirectionChange = 0;
-        }
+        //timeIdle += Time.deltaTime;
+        //Debug.Log("Current timeIdle: " + timeIdle);
+
+        //timeMoving += Time.deltaTime;
+        //Debug.Log("Current timeMoving: " + timeMoving);
+        //Timers Update here ends
+
+
 
         distance = Vector2.Distance(transform.position, player.transform.position); // Adquirir a distancia entre os objectos
         Vector2 direction = player.transform.position - transform.position; // Calculo do vector direcional deste objecto ao seu target (player)
@@ -38,30 +50,43 @@ public class EnemieBehaviour : MonoBehaviour
 
         float playerAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg; // Calculo do angulo entre dois pontos, aqui usamos a variavel direction para nos orientarmos na sua direção
         float randomAngle = Mathf.Atan2(randomDirection.y, randomDirection.x) * Mathf.Rad2Deg;
-        //transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-        //transform.rotation = Quaternion.Euler(Vector3.forward * angle);
 
-
-
+        if (timeSinceLastDirectionChange >= directionChangeInterval)
+        {
+            ChangeRandomDirection();
+            timeSinceLastDirectionChange = 0;
+        }
 
         if (distance < AggroRange)
         {
             Debug.Log("I See player");
-            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-            transform.rotation = Quaternion.Euler(Vector3.forward * playerAngle);
+            PlayerInAggroRange(playerAngle);
+            timeMoving = 6;
+            timeIdle = 0;
         }
         else
         {
-            Debug.Log("Where is that guy?");
-            
-            transform.position = Vector2.MoveTowards(
-                                                    this.transform.position,
-                                                    (Vector2)this.transform.position + randomDirection, //fazemos um cast para garantir que continuamos a ter um vector2 atribute
-                                                    speed * Time.deltaTime);
-            transform.rotation = Quaternion.Euler(Vector3.forward * randomAngle);
+            if (timeMoving < 6)
+            {
+                IdleMovement(randomAngle);
+            }
+            else
+            {
+                Debug.Log("I'm iddle");
+
+                timeIdle += Time.deltaTime;
+                if (timeIdle > 3)
+                {
+                    timeMoving = 0;
+                    timeIdle = 0;
+                }
+            }
         }
 
-        Debug.Log("Current distance: " + distance);
+        
+        
+        Debug.Log("timeIdle: "+timeIdle);
+        Debug.Log("timeMoving: " + timeMoving);
     }
 
     private void ChangeRandomDirection()
@@ -73,4 +98,23 @@ public class EnemieBehaviour : MonoBehaviour
 
         
     }
+
+    private void PlayerInAggroRange(float angle)
+    {
+        Debug.Log("I See player");
+        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+    }
+
+    private void IdleMovement(float angle)
+    {
+        transform.position = Vector2.MoveTowards(
+                                                    this.transform.position,
+                                                    (Vector2)this.transform.position + randomDirection, //fazemos um cast para garantir que continuamos a ter um vector2 atribute
+                                                    speed * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        timeMoving += Time.deltaTime;
+
+    }
+
 }
