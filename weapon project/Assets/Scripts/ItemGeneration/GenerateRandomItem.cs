cf04b8f95d04e3ef612141d4a7258c3dd2135caf
gig <101945826+ItzJustGig@users.noreply.github.com>
2025-01-21 21:16:48 +0000

    Added weights to each rarity for items
    55% - COMMON
    26% - UNCOMMON
    12% - RARE
    6% - EPIC
    1% - LEGENDARY

--- weapon project/Assets/Scripts/ItemGeneration/GenerateRandomItem.cs
+++ weapon project/Assets/Scripts/ItemGeneration/GenerateRandomItem.cs
@@ -1,7 +1,9 @@
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 public class GenerateRandomItem : MonoBehaviour
 {
@@ -17,11 +19,58 @@ public class GenerateRandomItem : MonoBehaviour
     {
         if (floorItem.item == null)
         {
-            List<Item> items = new List<Item>();
-            items = activeItemsGO.GetComponent<ItemList>().items.Union(passiveItemsGO.GetComponent<ItemList>().items).ToList();
-
-            floorItem.item = items[Random.Range(0, items.Count)];
-            floorItem.ForceStart();
+            Pick();
         }
+    }
+
+    public void Pick()
+    {
+        // Generate a random number between 0 and 100
+        float randomValue = UnityEngine.Random.Range(0f, 1f);
+        float[] probabilities = 
+        {
+            .55f,
+            .26f,
+            .12f,
+            .06f,
+            .01f
+        };
+
+        float val = 0;
+        Item.Rarity pick = Item.Rarity.COMMON;
+        for (int i = 0; i < probabilities.Count(); i++)
+        {
+            val += probabilities[i];
+            if (randomValue <= val)
+            {
+                switch (i)
+                {
+                    case 0:
+                        pick = Item.Rarity.COMMON; break;
+                    case 1:
+                        pick = Item.Rarity.UNCOMMON; break;
+                    case 2:
+                        pick = Item.Rarity.RARE; break;
+                    case 3:
+                        pick = Item.Rarity.EPIC; break;
+                    case 4:
+                        pick = Item.Rarity.LEGENDARY; break;
+                }
+                break;
+            }
+        }
+
+        List<Item> items = new List<Item>();
+        items = activeItemsGO.GetComponent<ItemList>().items.Union(passiveItemsGO.GetComponent<ItemList>().items).ToList();
+
+        for (int i = items.Count - 1; i >= 0; i--)
+        {
+            if (items[i].rarity != pick)
+                items.Remove(items[i]);
+        }
+
+        floorItem.item = items[UnityEngine.Random.Range(0, items.Count)];
+        floorItem.ForceStart();
+
     }
 }
