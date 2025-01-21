a02d438631d9587537355ed637cc12112cf54ce0
gig <101945826+ItzJustGig@users.noreply.github.com>
2025-01-21 14:47:14 +0000

    EventManager instructions

--- weapon project/Assets/EventManager.cs
+++ weapon project/Assets/EventManager.cs
@@ -3,6 +3,29 @@ using UnityEngine;
 
 public class EventManager : MonoBehaviour
 {
+    /*
+     * HOW TO USE
+     * ---
+     * NESTE SCRIPT
+     * public static event Action EventName; <- criar evento
+     * 
+     * public void EventTriggered() <- criar uma funcao neste script para dar trigger ao evento
+     * {
+     *     EventName?.Invoke();
+     * }
+     * 
+     * ---
+     * ONDE SE QUER QUE O EVENTO SE APLIQUE (ver ElectricBullet.cs para exemplo)
+     * EventManager.EventName += Funcao; <- dar attach de uma funcao ao evento, pode ser corrido num Start or similar, 
+     *                                      mas tem de correr esta parte do codigo.
+     * EventManager.EventName -= Funcao; <- num OnDestroy correr este codigo, pra evitar leaks de memoria
+     * 
+     * public void Funcao(){
+     *   *CODIGO QUE VAI CORRER QUANDO O EVENTO EVENTNAME DER TRIGGER*
+     * }
+     * ---
+    */
+
     public static event Action OnItemPickedUp;
     public static event Action<GameObject> OnBulletFired;
     public static event Action OnEnterNewRoom;
