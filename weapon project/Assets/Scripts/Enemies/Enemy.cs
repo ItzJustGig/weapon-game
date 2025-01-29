fc48480a0c35d51effaeb3c0f4cb8eb65db3fe0b
Eusébio Alves <eusebio.a95@hotmail.com>
2025-01-29 11:18:17 +0000

    Logic for base enemy

--- weapon project/Assets/Scripts/Enemies/Enemy.cs
+++ weapon project/Assets/Scripts/Enemies/Enemy.cs
@@ -1,16 +1,40 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    static public float maxHealth = 5;
+
+    private float currentHealth;
+
+    private void Start()   
     {
-        
+        currentHealth = maxHealth;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Projectile"))
+        {
+            Projectile projectile = other.gameObject.GetComponent<Projectile>(); // Referencia à bala que bateu neste indivíduo
+            
+            if (projectile.owner != this.gameObject) // Outros inimigos podem dar dano a inimigos? Mas não a eles próprios
+                TakeDamage(projectile.damage);
+        }
+    }
+
+    public void TakeDamage(float amount)
     {
-        
+        Debug.Log($"{name} took {amount} damage and has {currentHealth} HP.");
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        EventManager.Instance.EnemyKilled(); // Avisa o peeps que morri
+        Destroy(this.gameObject);
     }
 }
